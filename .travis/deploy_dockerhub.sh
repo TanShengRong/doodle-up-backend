4ff77e605ad04eeeb7fa1767437dd3d219a8f8b9
TanShengRong <tansr2010@gmail.com>
2020-10-07 18:58:39 +0800

    Update deploy_dockerhub.sh 1

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,9 @@
 docker login --username $DOCKER_USER --password $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-TAG="latest"
-else
-TAG="$TRAVIS_BRANCH"
-fi
-docker build -f Dockerfile -t $DOCKER_REPO .
-docker tag $LOWERCASE_REPO_SLUG:$TAG $DOCKER_REPO
+# if [ "$TRAVIS_BRANCH" = "master" ]; then
+# TAG="latest"
+# else
+# TAG="$TRAVIS_BRANCH"
+# fi
+docker build -f Dockerfile -t $LOWERCASE_REPO_SLUG:$TAG .
+docker tag $LOWERCASE_REPO_SLUG $DOCKER_REPO
 docker push $DOCKER_REPO
