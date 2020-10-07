e1a338a488624621eb2e6a28769e834b883c767a
TanShengRong <tansr2010@gmail.com>
2020-10-07 16:37:04 +0800

    Change TRAVIS_REPO_SLUG to lower 5

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,6 +5,5 @@ else
 TAG="$TRAVIS_BRANCH"
 fi
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-travis_repo_slug =`echo "$TRAVIS_REPO_SLUG" | tr '[:upper:]' '[:lower:]'` 
-docker tag $travis_repo_slug $DOCKER_REPO
+docker tag $LOWERCASE_REPO_SLUG $DOCKER_REPO
 docker push $DOCKER_REPO
