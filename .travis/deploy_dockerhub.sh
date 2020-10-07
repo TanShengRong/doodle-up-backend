0b9f6bccae0b94c865a406c0d8d339b9261344a0
TanShengRong <tansr2010@gmail.com>
2020-10-07 21:08:17 +0800

    Update deploy_dockerhub.sh 4

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -4,6 +4,7 @@ docker login --username $DOCKER_USER --password $DOCKER_PASS
 # else
 # TAG="$TRAVIS_BRANCH"
 # fi
-docker build -f Dockerfile -t $LOWERCASE_REPO_SLUG:latest .
-docker tag $LOWERCASE_REPO_SLUG:latest $DOCKER_USER/dockerhub:$LOWERCASE_REPO_SLUG
-docker push $DOCKER_USER/dockerhub:$LOWERCASE_REPO_SLUG
+docker build -f Dockerfile -t doodle-up-backend .
+docker tag doodle-up-backend:latest $DOCKER_USER/dockerhub:doodle-up-backend
+# docker push $DOCKER_USER/dockerhub:$LOWERCASE_REPO_SLUG
+docker push tansr77/dockerhub:doodle-up-backend
