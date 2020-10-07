15e596561cef5b11d9647d113e549a2430dd4e3c
TanShengRong <tansr2010@gmail.com>
2020-10-07 15:59:46 +0800

    Update deploy_dockerhub.sh

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,5 @@
 docker login --username $DOCKER_USER --password $DOCKER_PASS
-if ["TRAVIS_BRANCH" = "master"]; then
+if [ "$TRAVIS_BRANCH" = "master" ]; then
 TAG="latest"
 else
 TAG="$TRAVIS_BRANCH"
