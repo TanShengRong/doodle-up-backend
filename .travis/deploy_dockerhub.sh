9c647e13f6ec828b4720bf789d5f5e7a883a0e67
TanShengRong <tansr2010@gmail.com>
2020-10-07 21:00:07 +0800

    Update deploy_dockerhub.sh 3
    https://stackoverflow.com/a/44982036

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,5 +5,5 @@ docker login --username $DOCKER_USER --password $DOCKER_PASS
 # TAG="$TRAVIS_BRANCH"
 # fi
 docker build -f Dockerfile -t $LOWERCASE_REPO_SLUG:latest .
-docker tag $LOWERCASE_REPO_SLUG $DOCKER_REPO
-docker push $DOCKER_REPO
+docker tag $LOWERCASE_REPO_SLUG:latest $DOCKER_USER/dockerhub:$LOWERCASE_REPO_SLUG
+docker push $DOCKER_USER/dockerhub:$LOWERCASE_REPO_SLUG
