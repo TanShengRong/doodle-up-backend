64d2ac9fd0c1d4248e50be0ce5272187fba0dbb1
TanShengRong <tansr2010@gmail.com>
2020-10-07 16:44:39 +0800

    Change TRAVIS_REPO_SLUG to lower 6

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -4,6 +4,6 @@ TAG="latest"
 else
 TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker build -f Dockerfile -t $LOWERCASE_REPO_SLUG:$TAG .
 docker tag $LOWERCASE_REPO_SLUG $DOCKER_REPO
 docker push $DOCKER_REPO
