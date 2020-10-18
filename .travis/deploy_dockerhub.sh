efb3db41c29f271af3a8ebfac1a59c9ace30e4a8
TanShengRong <tansr2010@gmail.com>
2020-10-19 02:18:13 +0800

    remove selective deployment
    test another day

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-if [ "$TRAVIS_BRANCH" != "master" ] && [ $TRAVIS_PULL_REQUEST = "false" ]
-then
-  echo "we're not on master branch"
-  exit 0
-else
-  docker login --username $DOCKER_USER --password $DOCKER_PASS
-  docker build -f Dockerfile -t $DOCKER_TAGN .
-  docker tag $DOCKER_TAGN:latest $DOCKER_USER/$DOCKER_REPO:$DOCKER_TAGN
-  docker push $DOCKER_USER/$DOCKER_REPO:$DOCKER_TAGN
-fi
+# if [ "$TRAVIS_BRANCH" != "master" ] && [ $TRAVIS_PULL_REQUEST = "false" ]
+# then
+#   echo "we're not on master branch"
+#   exit 0
+# else
+# fi
+docker login --username $DOCKER_USER --password $DOCKER_PASS
+docker build -f Dockerfile -t $DOCKER_TAGN .
+docker tag $DOCKER_TAGN:latest $DOCKER_USER/$DOCKER_REPO:$DOCKER_TAGN
+docker push $DOCKER_USER/$DOCKER_REPO:$DOCKER_TAGN
--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-if [ $TRAVIS_BRANCH != "master" ] && [ $TRAVIS_PULL_REQUEST = "false" ]
-then
-  echo "we're not on master branch"
-  exit 0
-else
-  wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-  heroku plugins:install @heroku-cli/plugin-container-registry
-  docker login --username _ --password=$HEROKU_API_KEY registry.heroku.com
-  heroku container:push web --app doodle-up-backend
-  heroku container:release web --app doodle-up-backend
-fi
+# if [ $TRAVIS_BRANCH != "master" ] && [ $TRAVIS_PULL_REQUEST = "false" ]
+# then
+#   echo "we're not on master branch"
+#   exit 0
+# else
+# fi
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku plugins:install @heroku-cli/plugin-container-registry
+docker login --username _ --password=$HEROKU_API_KEY registry.heroku.com
+heroku container:push web --app doodle-up-backend
+heroku container:release web --app doodle-up-backend
