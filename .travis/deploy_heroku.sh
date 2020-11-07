51b59e3f67450ccd962ff5354d3a98ec366cff80
TanShengRong <tansr2010@gmail.com>
2020-11-07 19:06:18 +0800

    add condition in heroku deploy
    dont deploy if not master
    dont deploy if it is a pr

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -4,6 +4,10 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     echo "Should not deploy pull request; just doing a build."
     exit 0
 fi
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+    echo "Should not deploy pull request; just doing a build."
+    exit 0
+fi
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install @heroku-cli/plugin-container-registry
 docker login --username _ --password=$HEROKU_API_KEY registry.heroku.com
