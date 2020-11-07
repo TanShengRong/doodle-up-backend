059b312114fb2b91976cf0303888093dff92bae8
TanShengRong <tansr2010@gmail.com>
2020-11-07 19:16:59 +0800

    Branch unit testing (#4)
    * try unit testing on branch
    
    * fix .yml
    
    * add condition in heroku deploy
    
    dont deploy if not master
    dont deploy if it is a pr
    
    * fix echo

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-# if [ $TRAVIS_BRANCH != "master" ] && [ $TRAVIS_PULL_REQUEST = "false" ]
-# then
-#   echo "we're not on master branch"
-#   exit 0
-# else
-# fi
+# Pull requests shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo "Should not deploy pull request; just doing a build."
+    exit 0
+fi
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+    echo "Should not deploy a non-master branch; just doing a build."
+    exit 0
+fi
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install @heroku-cli/plugin-container-registry
 docker login --username _ --password=$HEROKU_API_KEY registry.heroku.com
