e8288bca38ec96f4cb2e06e798e8334ef4e91070
TanShengRong <tansr2010@gmail.com>
2020-11-07 18:40:50 +0800

    add initial pytest

--- test_api.py
+++ test_api.py
@@ -1,25 +1,54 @@
-import api
-import os
-import tempfile
+import requests
+from api import app as flask_app
 import pytest
 
 @pytest.fixture
-def client():
-    api.app.config['TESTING'] = True
-    # api.app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+def app():
+    yield flask_app
 
-    with api.app.test_client() as client:
-        with api.app.app_context():
-            # flaskr.init_db()
-            api.app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
-        yield client
 
-    # os.close(db_fd)
-    # os.unlink(flaskr.app.config['DATABASE'])
+@pytest.fixture
+def client(app):
+    return app.test_client()
 
-def test_progress(client):
-    """Start with a blank database."""
-    rv = client.get('/progress?username=jack&storyid=001')
-    assert rv.data
-    # rv = client.get('/')
-    # assert b'No entries here so far' in rv.data
+def test_index(client):
+    res = client.get('/')
+    assert res.status_code == 200
+    expected = """
+<pre>
+ ######                                     #     #           ######                                            
+ #     #  ####   ####  #####  #      ###### #     # #####     #     #   ##    ####  #    # ###### #    # #####  
+ #     # #    # #    # #    # #      #      #     # #    #    #     #  #  #  #    # #   #  #      ##   # #    # 
+ #     # #    # #    # #    # #      #####  #     # #    #    ######  #    # #      ####   #####  # #  # #    # 
+ #     # #    # #    # #    # #      #      #     # #####     #     # ###### #      #  #   #      #  # # #    # 
+ #     # #    # #    # #    # #      #      #     # #         #     # #    # #    # #   #  #      #   ## #    # 
+ ######   ####   ####  #####  ###### ######  #####  #         ######  #    #  ####  #    # ###### #    # #####     
+ </pre> 
+"""
+    assert expected == res.get_data(as_text=True)
+
+def test_sign_in(client):
+    payload = {'password': '123456',
+    'email': 'pytest@gmail.com'}
+
+    response = client.post(
+        '/signin', 
+        data = payload, 
+        follow_redirects = True
+    )
+    assert response.status_code == 200
+    assert b'pytest' in response.data
+
+def test_sign_up(client):
+    payload = {'username': 'pytest',
+    'password': '123456',
+    'email': 'pytest@gmail.com'}
+
+    response = client.post(
+        '/signup',
+        data = payload,
+        follow_redirects = True
+    )
+    # account already created, should return "The email is already in use", 400
+    assert response.status_code == 400
+    assert b'The email is already in use' in response.data
